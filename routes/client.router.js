55e6ca3a77190a098488020936966958088005d9
anggadsatria <angga.dsatria27@gmail.com>
2022-04-29 02:03:49 +0700

    remove whitespace

--- routes/client.router.js
+++ routes/client.router.js
@@ -2,7 +2,6 @@ const express = require(`express`);
 const router = express.Router();
 const clientController = require(`../controllers/client.controller`)
 
-
 router.get(`/`, clientController.getAllClient);
 router.get(`/:id`, clientController.getClientById);
 router.post(`/create`, clientController.createClient)
