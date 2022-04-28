d3ab0fb96c9d79bf0ba66b5b94595f0059a1edf1
anggadsatria <angga.dsatria27@gmail.com>
2022-04-28 16:35:21 +0700

    add 3 associations

--- models/brand.js
+++ models/brand.js
@@ -9,8 +9,9 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate(models) {
+    static associate({client}) {
       // define association here
+      this.belongsTo(client, {foreignKey: `client_id`}) //client has many brand
     }
   }
   brand.init({
--- models/client_type.js
+++ models/client_type.js
@@ -9,8 +9,9 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate(models) {
+    static associate({client}) {
       // define association here
+      this.belongsTo(client, {foreignKey: `client_type_id`}) //client has client type
     }
   }
   client_type.init({
